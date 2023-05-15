0df66ff16bf581e29d97c26717041a8a949091ab
Ayush Patel <ayush.patel@marutitech.com>
2023-05-15 14:36:49 +0530

    DS-Assignemt1
    monitoring file system

---
+++ main.py
@@ -0,0 +1,47 @@
+import random
+import asyncio
+import aiofiles as aiofiles
+import re
+
+
+def generate_random_word():
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if random.random() > 0.5:
+        random_str = 'MARUTI'
+    else:
+        random_str = ''.join(random.choice(letters) for i in range(6))
+    return random_str
+
+
+async def write_in_file():
+    random_str = generate_random_word()
+    async with aiofiles.open('file1.txt', mode='a') as f1, aiofiles.open('file2.txt', mode='a') as f2:
+        await f1.write(random_str + "\n")
+        await asyncio.sleep(2)
+        await f2.write(random_str + "\n")
+
+
+async def read_in_file():
+    async with aiofiles.open('file1.txt', mode='r') as f1, aiofiles.open('file2.txt', mode='r') as f2:
+        lines1 = await f1.read()
+        lines2 = await f2.read()
+        findWord = r"\bMARUTI\b"
+
+        count1 = len(re.findall(findWord, lines1))
+        count2 = len(re.findall(findWord, lines2))
+    return count1, count2
+
+
+async def write_in_logfile(count1, count2):
+    async with aiofiles.open('counts.log', mode="a") as logs:
+        await logs.write(f"MARUTI word in file1 have {count1} times and file2 have {count2} times\n")
+
+
+async def main():
+    await write_in_file()
+    count1, count2 = await read_in_file()
+    await write_in_logfile(count1, count2)
+    print("monitor file run successfully")
+
+
+asyncio.run(main())
